217b81c332fc2c1de677feae92231b3ee2ab54c3
kylrth <kylrth@gmail.com>
2019-05-17 15:15:29 -0500

    PEP8 and docstrings for Constants.py

--- pyMode/Constants.py
+++ pyMode/Constants.py
@@ -1,5 +1,10 @@
-import pyMode as pm
+"""Constant definitions to be used by other code.
+
+Alec Hammond. 2019-02-16.
+"""
+
 from collections import namedtuple
+import pyMode as pm
 
 
 FreqRange = namedtuple('FreqRange', ['min', 'max'])
